c9d05bd7ef756fce2f72918fad7419fb72129ad9
Alves0611 <g.alves0611@gmail.com>
2024-01-31 01:52:39 -0300

    add versioning

--- 01-terraform-basic/s3.tf
+++ 01-terraform-basic/s3.tf
@@ -1,5 +1,12 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "exemple" {
   bucket = "my-tf-studying-bucket-0611"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
