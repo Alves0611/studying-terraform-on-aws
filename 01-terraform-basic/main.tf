f4814243629620277f9724f149eeaa9980458cbe
Alves0611 <g.alves0611@gmail.com>
2024-01-31 00:45:04 -0300

    add links

--- 01-terraform-basic/main.tf
+++ 01-terraform-basic/main.tf
@@ -1,6 +1,7 @@
+#https://developer.hashicorp.com/terraform/intro
+#https://developer.hashicorp.com/terraform;language/providers
+#https://developer.hashicorp.com/terraform;language/resources
 
-
-# Configure the AWS Provider
 provider "aws" {
   region  = "us-east-1"
   profile = "studying"
