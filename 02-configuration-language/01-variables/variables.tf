0cd958359d903ce55d89e6811b72e23307a2c7bc
Alves0611 <g.alves0611@gmail.com>
2024-02-01 00:30:18 -0300

    add validation

--- 02-configuration-language/01-variables/variables.tf
+++ 02-configuration-language/01-variables/variables.tf
@@ -2,6 +2,11 @@ variable "aws_region" {
   type        = string
   description = "The region to deploy the infra to"
   default     = "us-east-1"
+
+  validation {
+    condition     = can(regex("^[a-z][a-z]-[a-z]+-[0-9]+$", var.aws_region))
+    error_message = "Invalid AWS region name"
+  }
 }
 
 variable "aws_profile" {
