0fdf67e4ec133eacfd6baafe00a5d11fd628657b
Alves0611 <g.alves0611@gmail.com>
2024-02-02 01:55:31 -0300

    add main.tf

---
+++ 02-configuration-language/03-outputs/main.tf
@@ -0,0 +1,14 @@
+# https://developer.hashicorp.com/terraform/language/values/outputs
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "studying"
+
+  default_tags {
+    tags = {
+      "ManagedBy" = "terraform"
+    }
+  }
+}
+
+
