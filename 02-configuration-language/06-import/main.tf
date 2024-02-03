db6ced140db6fec9eb91dea3c6ba195b1fc34fbf
Alves0611 <g.alves0611@gmail.com>
2024-02-03 19:56:15 -0300

    add main.tf

---
+++ 02-configuration-language/06-import/main.tf
@@ -0,0 +1,14 @@
+# https://developer.hashicorp.com/terraform/cli/import
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "studying"
+
+  default_tags {
+    tags = {
+      "ManagedBy" = "terraform"
+    }
+  }
+}
+
+
