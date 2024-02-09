8b5bf868418635866fc60fc65ec1a447f829516d
Alves0611 <g.alves0611@gmail.com>
2024-02-09 19:37:39 -0300

    add s3.tf

---
+++ 03-remote-state/01-backend/s3.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "remote_state" {
+  bucket = "tfstate-${data.aws_caller_identity.current.account_id}"
+}
+
+resource "aws_s3_bucket_versioning" "remote_state" {
+  bucket = aws_s3_bucket.remote_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
