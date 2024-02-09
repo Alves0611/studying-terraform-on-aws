44ce3cc16445eb13ed6960aea650e4ba72f2d227
Alves0611 <g.alves0611@gmail.com>
2024-02-09 19:36:09 -0300

    add main.tf

---
+++ 02-configuration-language/14-for_splat_expressions/main.tf
@@ -0,0 +1,11 @@
+# https://developer.hashicorp.com/terraform/language/expressions/for
+provider "aws" {
+  region  = "us-east-1"
+  profile = "studying"
+
+  default_tags {
+    tags = {
+      "ManagedBy" = "terraform"
+    }
+  }
+}
