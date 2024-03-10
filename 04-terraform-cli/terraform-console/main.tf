88cff9e4c346886d38b36eee9e5f5036e8ee885e
Alves0611 <g.alves0611@gmail.com>
2024-03-09 22:43:48 -0300

    studying terraform console

---
+++ 04-terraform-cli/terraform-console/main.tf
@@ -0,0 +1,20 @@
+# https://developer.hashicorp.com/terraform/cli/commands/console
+
+variable "apps" {
+  type = map(any)
+  default = {
+    "foo" = {
+      "region" = "us-east-1",
+    },
+    "bar" = {
+      "region" = "eu-west-1",
+    },
+    "baz" = {
+      "region" = "ap-south-1",
+    },
+  }
+}
+
+resource "random_pet" "example" {
+  for_each = var.apps
+}
